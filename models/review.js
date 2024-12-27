ba5ed6580c5d53fe829b1ff5d970f573099b442c
mujtabasaqib <mujtabasaqib99@gmail.com>
2024-12-27 14:40:23 +0500

    added more validation for schemas

--- models/review.js
+++ models/review.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
 const reviewSchema = new mongoose.Schema({
-  attraction: { type: mongoose.Schema.Types.ObjectId, ref: 'Attraction' },
-  visitor: { type: mongoose.Schema.Types.ObjectId, ref: 'Visitor' },
+  attraction: { type: mongoose.Schema.Types.ObjectId, ref: 'Attraction', required: true },
+  visitor: { type: mongoose.Schema.Types.ObjectId, ref: 'Visitor', required: true },
   score: {
     type: Number,
     min: 1,
@@ -13,5 +13,8 @@ const reviewSchema = new mongoose.Schema({
     type: String
   }
 })
+
+//a visitor cannot reviw the same attraction twice
+reviewSchema.index({ visitor: 1, attraction: 1 }, { unique: true });
 
 module.exports = mongoose.model('Review', reviewSchema);
--- models/visitor.js
+++ models/visitor.js
@@ -7,6 +7,7 @@ const visitorSchema = new mongoose.Schema({
     },
     email: {
       type: String,
+      unique: true,
       required: true,
       validate: {
         validator: function (value) {
